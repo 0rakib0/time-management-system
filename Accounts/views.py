1e7e0a3fceadc958f93165b1d7e269a439a19a3f
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-10 10:22:28 +0600

    create login view

--- Accounts/views.py
+++ Accounts/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
     return render(request, 'Accounts/Login.html')
