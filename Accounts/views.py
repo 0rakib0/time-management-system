48c61456a293896f00b65bda2ee9c6522788ce34
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-10 10:36:20 +0600

    complate login and add django message file

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,  redirect
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 def Login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username, password)
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('Dashbord:dashbord')
+        else:
+            print('User not Found')
     return render(request, 'Accounts/Login.html')
