88cd043a41cdcf070700fe7d8f65ae4ad3fce4d9
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-15 21:39:37 +0600

    create skill models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Skill(models.Model):
+    skill_name = models.CharField(max_length=120)
+    skill_icon = models.CharField(max_length=120)
+    create_att = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.skill_name
