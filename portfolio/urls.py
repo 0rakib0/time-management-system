ec377c5eac55093959807540d0b0e6f07fe0b48f
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-15 21:20:02 +0600

    create add skill view and url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'portfolio'
 
 urlpatterns = [
-    
+    path('add-skills/', views.add_skills, name='add_skill')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def add_skills(request):
-    return render(request, '')
+    return render(request, 'portfolio/addSkills.html')
